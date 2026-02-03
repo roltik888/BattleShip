49f0e3168252842452c131ee2cd7400a856e5c17
roltik888 <dzgofs@gmail.com>
2026-02-03 09:26:48 +0100

    Update Program.cs
    tablica + deklaracja

--- Statki/Program.cs
+++ Statki/Program.cs
@@ -1,2 +1,14 @@
-wjuykju
-
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int woda = 0;
+            int statek = 1;
+            int shoot = 2;
+            int trafiony = 3;
+            int[,] pole = new int[10,10];
+        }
+    }
+}
